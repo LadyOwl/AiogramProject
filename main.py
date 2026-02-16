6d3ee33404665543ae20e96c628b67ea8053d52c
Юлия (Джулия) <sixquestions666@gmail.com>
2026-02-16 14:14:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from config import TOKEN
 
+import random
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+@dp.message (F.photo)
+async def aitext(message: Message):
+    list = ['Классное фото!', 'Огонь!', 'Непонятно!']
+    rand_ans = random.choice(list)
+    await message.answer(rand_ans)
+
+
+@dp.message (F.text == 'Что такое ИИ?')
+async def aitext(message: Message):
+    await message.answer('Искусственный интеллект — компьютерные системы, имитирующие человеческое мышление.')
 
 @dp.message(Command('help'))
 async def help(message: Message):
