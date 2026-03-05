955c5a2454e118b3d39a49e5b496383ae27a3d04
Юлия (Джулия) <sixquestions666@gmail.com>
2026-03-05 17:03:52 +0300

    Update new.py

--- new.py
+++ new.py
@@ -1,10 +1,25 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import TOKEN
 import sqlite3
+import aiohttp
 
-conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY, 
-    username TEXT,
-    chat_id INTEGER)''')
+
+
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
