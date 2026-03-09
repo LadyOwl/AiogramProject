450c6e2beeef7b43af72f2e82a76d7ced8264e27
Юлия (Джулия) <sixquestions666@gmail.com>
2026-03-09 11:18:27 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -3,4 +3,5 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 main = ReplyKeyboardMarkup(keyboard=[
    [KeyboardButton(text="Тестовая кнопка 1")],
    [KeyboardButton(text="Тестовая кнопка 2"), KeyboardButton(text="Тестовая кнопка 3")]
-])
+]),
+resize_keyboard=True
