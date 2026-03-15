c1be1fd4c575a6b4880ac631cd23de0babd38e27
Юлия (Джулия) <sixquestions666@gmail.com>
2026-03-15 15:49:27 +0300

    Update cats.py

--- cats.py
+++ cats.py
@@ -3,11 +3,32 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 import random
+import requests
 
-from config import TOKEN
+from config import TOKEN, THE_CAT_API_KEY
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+def get_cat_breeds():
+    url = 'https://api.thecatapi.com/v1/breeds'
+    headers = {'x-api-key': THE_CAT_API_KEY}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def get_cat_image_by_breed(breed_id):
+    url = f'https://api.thecatapi.com/v1/images/search?breed_ids={breed_id}'
+    headers = {'x-api-key': THE_CAT_API_KEY}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data[0]['url']
+
+def get_breed_info(breed_name):
+    breeds = get_cat_breeds()
+    for breed in breeds:
+        if breed['name'].lower == breed_name.lower():
+            return breed
+    return None
 
 
 
