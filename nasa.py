7b75e5dc882beac712b9f68c7acec0d2d892b756
Юлия (Джулия) <sixquestions666@gmail.com>
2026-03-18 18:26:30 +0300

    Update nasa.py

--- nasa.py
+++ nasa.py
@@ -2,6 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from datetime import datetime, timedelta
 import random
 import requests
 
@@ -10,6 +11,14 @@ from config import TOKEN, NASA_API_KEY
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+def get_random_apod():
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=365)
+    random_date = start_date + (end_date - start_date) * random.random()
+    date_str = random_date.strftime("%Y-%m-%d")
+    url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}&date={date_str}"
+    response = requests.get(url)
+    return response.json()
 
 async def main():
    await dp.start_polling(bot)
