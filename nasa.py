901473f10ae1cc2323d1b204963740cf654dcf9e
Юлия (Джулия) <sixquestions666@gmail.com>
2026-03-18 20:43:15 +0300

    Update nasa.py

--- nasa.py
+++ nasa.py
@@ -20,6 +20,16 @@ def get_random_apod():
     response = requests.get(url)
     return response.json()
 
+@dp.message(Command('random_apod'))
+async def random_apod(message: Message):
+    apod = get_random_apod()
+    photo_url = apod['url']
+    title = apod['title']
+
+
+    await message.answer_photo(photo=photo_url, caption=f"{title}")
+
+
 async def main():
    await dp.start_polling(bot)
 
