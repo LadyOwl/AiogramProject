c9e3301a1eab61267e6e32ddedc597504b8b181d
Юлия (Джулия) <sixquestions666@gmail.com>
2026-03-18 17:57:57 +0300

    Update nasa.py

---
+++ nasa.py
@@ -0,0 +1,27 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+import random
+import requests
+
+from config import TOKEN, THE_CAT_API_KEY
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+
+
+
+
+
+
+
+
+
+async def main():
+   await dp.start_polling(bot)
+
+if __name__ == '__main__':
+   asyncio.run(main())
